8c386777ff82f48ed0365b416274ac90ae705617
Ferhat <116122505+falakus10@users.noreply.github.com>
2024-07-26 00:02:12 +0300

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: falakus <falakus@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/20 14:27:23 by falakus           #+#    #+#             */
+/*   Updated: 2024/07/20 14:27:24 by falakus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		ft_putchar(str[i++]);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		if (s1[i] < s2[i] || s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	main(int argc, char **argv)
+{
+	int		j;
+	int		x;
+
+	x = 1;
+	while (x < argc - 1)
+	{
+		j = 1;
+		while (j < argc - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				ft_swap(&argv[j], &argv[j + 1]);
+			}
+			j++;
+		}
+		x++;
+	}
+	j = 1;
+	while (j < argc)
+	{
+		ft_putstr(argv[j]);
+		write (1, "\n", 1);
+		j++;
+	}
+	return (0);
+}
