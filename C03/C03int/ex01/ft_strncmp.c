6641d94afbef5fc935cf854be1648df58747a97a
Ferhat <116122505+falakus10@users.noreply.github.com>
2024-07-26 00:00:14 +0300

    Add files via upload

---
+++ C03/C03int/ex01/ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: falakus <falakus@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 00:16:47 by falakus           #+#    #+#             */
+/*   Updated: 2024/07/16 00:17:35 by falakus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+int main()
+{
+	char str1[] = "abcdeefakfn";
+	char str2[] = "abcdeefbfgerfv";
+	printf("%d", ft_strncmp(str1, str2, 7));
+}
