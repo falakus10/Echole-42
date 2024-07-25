a99eea1ba9182920095dc28cd171a5f92f8105db
Ferhat <116122505+falakus10@users.noreply.github.com>
2024-07-25 23:54:21 +0300

    Add files via upload

---
+++ C00/ex04/ft_is_negative.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: falakus <falakus@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/06 10:02:28 by falakus           #+#    #+#             */
+/*   Updated: 2024/07/08 14:07:10 by falakus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	char	positive;
+	char	negative;
+
+	positive = 'P';
+	negative = 'N';
+	if (n >= 0)
+	{
+		write(1, &positive, 1);
+	}
+	else
+	{
+		write(1, &negative, 1);
+	}
+}
---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: falakus <falakus@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 11:39:43 by falakus           #+#    #+#             */
+/*   Updated: 2024/07/08 14:07:04 by falakus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
